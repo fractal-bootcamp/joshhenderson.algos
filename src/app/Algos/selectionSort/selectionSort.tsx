dc8f3086194fe73d0b32e8f41286ea7c9e5012ae
Joshua Henderson-Jardim <hendersonjardimj@gmail.com>
2024-06-28 17:52:07 -0400

    selection sort working

--- src/app/Algos/selectionSort/selectionSort.tsx
+++ src/app/Algos/selectionSort/selectionSort.tsx
@@ -2,19 +2,27 @@ import React from 'react'
 
 const initialArray = [95, 65, 45, 23, 57, 89, 12, 45, 67, 34, 78, 56, 90]
 
-
+console.log("here")
 export default function selectionSort(array: number[]) {
     const arrayCopy = array
-    let sortedArray = []
-    array.forEach((element) => {
-        if (arrayCopy.length != 0 && element + 1 < element) {
-            console.log(arrayCopy)
-            sortedArray = arrayCopy.splice(element, 1, element)
 
-            console.log(sortedArray)
+    for (let i = 0; i < array.length - 1; i++) {
+        let minIndex = i // set the minIndex cutoff where the algo inserts/swaps the next smallest value in the array
+        console.log("1st here")
+        for (let j = i + 1; i < array.length; i++) { //second for loop that 
+            console.log("2nd here")
+            const smallestNumber = Math.min(...arrayCopy)
+            console.log(smallestNumber)
+            const smallestNumIndex = arrayCopy.findIndex((e) => e === smallestNumber)
+            return (console.log(arrayCopy, smallestNumIndex))
+
         }
-    })
-    return selectionSort(initialArray)
+    }
+
+
 }
 
 selectionSort(initialArray)
+
+
+//[newArray[i], newArray[i + 1]] = [newArray[i + 1], newArray[i]]
